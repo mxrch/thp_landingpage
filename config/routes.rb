fe2b0ac17353b4c97ad18b1742fa7efc7cdf0d71
OwwllMan <doniermathieu@gmail.com>
2018-11-14 13:09:01 +0100

    Le COMMIT

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root 'home#index'
+
+  get 'FAQ', to: 'home#faq'
+  get 'Profil', to: 'home#profil'
+  devise_for :users, skip: [:sessions]
+  as :user do
+    get 'login', to: 'devise/sessions#new', as: :new_user_session
+    post 'login', to: 'devise/sessions#create', as: :user_session
+    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+end
